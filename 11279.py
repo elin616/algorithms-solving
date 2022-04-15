819ea4ce673c2ab0d70fd17110a9edaa6d5cc389
elin616 <dksguswn97@nate.com>
2022-04-15 11:55:16 +0900

    11279풀이

---
+++ 11279.py
@@ -0,0 +1,15 @@
+import heapq
+import sys
+
+n = int(sys.stdin.readline())
+heap = []
+
+for _ in range(n):
+    num = int(sys.stdin.readline())
+    if num != 0:
+        heapq.heappush(heap,-num)
+    else :
+        if len(heap) == 0 :
+            print(0)
+        else :
+            print(-heapq.heappop(heap))
